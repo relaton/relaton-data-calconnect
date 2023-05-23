f8d65503fc8c5555a5cc7dce7628718efa85ce56
Alexande B <abobrikovich@gmail.com>
2023-05-23 11:07:40 +0200

    Simplify crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,6 @@
 require 'fileutils'
 
-t1 = Time.now
-puts "Started at: #{t1}"
-
 FileUtils.rm_rf("data")
-system("relaton fetch-data calconnect-org")
 
-t2 = Time.now
-puts "Stopped at: #{t2}"
-puts "Done in: #{(t2 - t1).round} sec."
+require 'relaton_calconnect'
+RelatonCalconnect::DataFetcher.fetch

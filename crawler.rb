a43219d9f52997cdab72333f602f537f299df23d
Andrei Kislichenko <android.2net@gmail.com>
2023-06-07 19:42:59 -0400

    Update crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -8,6 +8,3 @@ FileUtils.rm_rf("data")
 FileUtils.rm(Dir.glob("index*"))
 
 RelatonCalconnect::DataFetcher.fetch
-
-system "zip index-v1.zip index-v1.yaml"
-system "git add index-v1.zip index-v1.yaml"
